06937bcb885ba5956abe134fd5aa0d2cf56e4545
Henrique <henriquepassos2004@outlook.com.br>
2025-08-01 20:38:55 -0300

    Mapeamento da placa

---
+++ Mapeamento-placa/mapeamento/pcihello_restored/pcihellocore/pcihellocore_bb.v
@@ -0,0 +1,28 @@
+
+module pcihellocore (
+	hexport1_external_connection_export,
+	hexport_external_connection_export,
+	inport_external_connection_export,
+	ledg_external_connection_export,
+	ledr_external_connection_export,
+	pcie_hard_ip_0_pcie_rstn_export,
+	pcie_hard_ip_0_powerdown_pll_powerdown,
+	pcie_hard_ip_0_powerdown_gxb_powerdown,
+	pcie_hard_ip_0_refclk_export,
+	pcie_hard_ip_0_rx_in_rx_datain_0,
+	pcie_hard_ip_0_tx_out_tx_dataout_0,
+	button_external_connection_export);	
+
+	output	[31:0]	hexport1_external_connection_export;
+	output	[31:0]	hexport_external_connection_export;
+	input	[15:0]	inport_external_connection_export;
+	output	[31:0]	ledg_external_connection_export;
+	output	[31:0]	ledr_external_connection_export;
+	input		pcie_hard_ip_0_pcie_rstn_export;
+	input		pcie_hard_ip_0_powerdown_pll_powerdown;
+	input		pcie_hard_ip_0_powerdown_gxb_powerdown;
+	input		pcie_hard_ip_0_refclk_export;
+	input		pcie_hard_ip_0_rx_in_rx_datain_0;
+	output		pcie_hard_ip_0_tx_out_tx_dataout_0;
+	input	[3:0]	button_external_connection_export;
+endmodule
